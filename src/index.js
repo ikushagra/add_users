e906a7ca654eb7aca789adae3ded063b7361d9f5
ikushagra <65224525+ikushagra@users.noreply.github.com>
2021-04-22 20:59:22 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,38 @@ const api = `https://randomuser.me/api`;
 const addUser = document.getElementById(`add-user-btn`);
 const mainApp = document.getElementById("app");
 
+const appState = [];
+const userList = document.getElementById("user-list");
+const searchInput = document.getElementById("search");
+
 async function foo() {
   const userData = await fetch(api, {
     method: "GET"
   });
   const userJson = await userData.json();
-  console.log(userJson.results[0]);
   const user = userJson.results[0];
-  const userEl = document.createElement("div");
-  userEl.innerHTML = `<div>
-${user.name.title} ${user.name.first} ${user.name.last}
-</div>`;
-  mainApp.appendChild(userEl);
+  appState.push(user);
+  console.log(appState);
+  domRenderer(appState);
 }
+
+const domRenderer = (stateArray) => {
+  userList.innerHTML = null;
+
+  stateArray.forEach((userObj) => {
+    const userEl = document.createElement("div");
+    userEl.innerHTML = `<div>
+  ${userObj.name.title} ${userObj.name.first} ${userObj.name.last}
+  </div>`;
+    userList.appendChild(userEl);
+  });
+};
+
+searchInput.addEventListener("keyup", () => {
+  const filteredAppState = appState.filter((user) =>
+    user.name.first.toLowerCase().includes(searchInput.value.toLowerCase())
+  );
+  domRenderer(filteredAppState);
+});
+
 addUser.addEventListener("click", foo);
