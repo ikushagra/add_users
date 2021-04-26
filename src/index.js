346be56a539edcf9e4c80ff983a49585e0314086
ikushagra <65224525+ikushagra@users.noreply.github.com>
2021-04-26 17:45:52 +0530

    added class

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ const mainApp = document.getElementById("app");
 const appState = [];
 const userList = document.getElementById("user-list");
 const searchInput = document.getElementById("search");
+const descSortBtn = document.getElementById("sort-desc");
+const ascSortBtn = document.getElementById("sort-asc");
+
+class User {
+  constructor(title, first, last, gender, email) {
+    this.name = `${first} ${last}`;
+    this.title = `${title}`;
+    this.gender = `${gender}`;
+    this.email = `${email}`;
+  }
+}
 
 async function foo() {
   const userData = await fetch(api, {
@@ -12,8 +23,10 @@ async function foo() {
   });
   const userJson = await userData.json();
   const user = userJson.results[0];
-  appState.push(user);
-  console.log(appState);
+  // appState.push(user);
+  const classUser = new User(user.name, user.title, user.gender, user.email);
+  appState.push(classUser);
+
   domRenderer(appState);
 }
 
@@ -23,17 +36,36 @@ const domRenderer = (stateArray) => {
   stateArray.forEach((userObj) => {
     const userEl = document.createElement("div");
     userEl.innerHTML = `<div>
-  ${userObj.name.title} ${userObj.name.first} ${userObj.name.last}
+  Name: ${userObj.name}
+  <ol>
+  <li>${userObj.gender}</li>
+  <li>${userObj.email}</li>
+  </ol>
   </div>`;
     userList.appendChild(userEl);
   });
 };
 
 searchInput.addEventListener("keyup", () => {
-  const filteredAppState = appState.filter((user) =>
-    user.name.first.toLowerCase().includes(searchInput.value.toLowerCase())
+  const filteredAppState = appState.filter(
+    (user) =>
+      user.name.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.gender.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchInput.value.toLowerCase())
   );
   domRenderer(filteredAppState);
+});
+
+descSortBtn.addEventListener("click", () => {
+  const appStateCopy = [...appState];
+  appStateCopy.sort((a, b) => (a.name < b.name ? 1 : -1));
+  domRenderer(appStateCopy);
+});
+
+ascSortBtn.addEventListener("click", () => {
+  const appStateCopy = [...appState];
+  appStateCopy.sort((a, b) => (a.name > b.name ? 1 : -1));
+  domRenderer(appStateCopy);
 });
 
 addUser.addEventListener("click", foo);
